5a97cc1524f0475fb69ea84ac0d1444b4a54a229
aguiot-- <aguiot--@student.42.fr>
2019-02-03 18:07:55 +0100

    Add Retry

--- src/client.c
+++ src/client.c
@@ -6,39 +6,35 @@
 /*   By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/02 15:53:27 by aguiot--          #+#    #+#             */
-/*   Updated: 2019/02/03 17:37:03 by aguiot--         ###   ########.fr       */
+/*   Updated: 2019/02/03 18:08:39 by aguiot--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	ft_send_seqcount(pid_t pid, int *bin)
+static void	ft_retry(pid_t pid, int sig)
 {
-	int		i;
+	static int	times = 0;
 
-	i = 0;
-	while (i < 8)
+	ft_putstr_fd(C_RED"Retrying in 3 seconds", 2);
+	sleep(1);
+	ft_putchar_fd('.', 2);
+	sleep(1);
+	ft_putchar_fd('.', 2);
+	sleep(1);
+	ft_putchar_fd('.', 2);
+	ft_putchar_fd('\n', 2);
+	if (kill(pid, sig) == -1)
 	{
-		if (bin[i] == 0)
-			if (kill(pid, SIGUSR1) == -1)
-			{
-				ft_putendl_fd(C_RED"An error occured while sending bit '0' \
-in seqcount."C_NONE, 2);
-				exit(1);
-			}
-		if (bin[i] == 1)
-			if (kill(pid, SIGUSR2) == -1)
-			{
-				ft_putendl_fd(C_RED"An error occured while sending bit '1' \
-in seqvalue."C_NONE, 2);
-				exit(1);
-			}
-		usleep(150);
-		++i;
+		++times;
+		if (times < 3)
+			ft_retry(pid, sig);
+		ft_putendl_fd(C_RED"Failed 3 times, quitting."C_NONE, 2);
+		exit(1);
 	}
 }
 
-static void	ft_send_seqvalue(pid_t pid, int *bin)
+static void	ft_send_char(pid_t pid, int *bin)
 {
 	int		i;
 
@@ -48,16 +44,16 @@ static void	ft_send_seqvalue(pid_t pid, int *bin)
 		if (bin[i] == 0)
 			if (kill(pid, SIGUSR1) == -1)
 			{
-				ft_putendl_fd(C_RED"An error occured while sending bit '0' \
+				ft_putendl_fd(C_RED"An error occured while sending bit '0'\
 ."C_NONE, 2);
-				exit(1);
+				ft_retry(pid, SIGUSR1);
 			}
 		if (bin[i] == 1)
 			if (kill(pid, SIGUSR2) == -1)
 			{
-				ft_putendl_fd(C_RED"An error occured while sending bit '1' \
+				ft_putendl_fd(C_RED"An error occured while sending bit '1'\
 ."C_NONE, 2);
-				exit(1);
+				ft_retry(pid, SIGUSR2);
 			}
 		usleep(150);
 		++i;
@@ -75,12 +71,12 @@ static int	ft_send_text(pid_t pid, char *s)
 		j = 0;
 		while (s[i + j] && s[i + j] == s[i])
 			++j;
-		ft_send_seqcount(pid, ft_dec_to_bin(j));
-		ft_send_seqvalue(pid, ft_dec_to_bin((int)s[i]));
+		ft_send_char(pid, ft_dec_to_bin(j));
+		ft_send_char(pid, ft_dec_to_bin((int)s[i]));
 		i += j;
 	}
-	ft_send_seqcount(pid, ft_dec_to_bin(1));
-	ft_send_seqvalue(pid, ft_dec_to_bin((int)'\0'));
+	ft_send_char(pid, ft_dec_to_bin(1));
+	ft_send_char(pid, ft_dec_to_bin((int)'\0'));
 	return (0);
 }
 
